b0da13c6b61065eacb93e3a6a561a5146844ae84
Zaya <noazaya6@gmail.com>
2025-06-25 16:11:21 -0300

    Exercicio 5.18 logica de prog.

--- 00-script.js
+++ 00-script.js
@@ -1,27 +1,11 @@
- // alert ("oi") 
- 
- //SQ2, ex3 - Dias da semana
- let numero 
+// Ler a distância da próxima corrida e dizer quanto tempo vai levar. 
+// // 1 dado de Entrada: Distância (em quilômetros); 
+// // 1 dado de Saída: Tempo de viagem (em segundos).
 
- numero = prompt ("Digite um numero de 1 à 7")
- if(numero == 1){
-    alert ("Hoje e domingo")
- }
-if(numero == 2){
-    alert ("Hoje e segunda")
-}
-if(numero == 3){
-    alert ("hoje e terca")
-}
-if(numero == 4){
-    alert ("hoje e quarta")
-}
-if (numero == 5){
-    alert ("hoje e quinta")
-}
-if (numero == 6){
-    alert ("hoje e sexta")
-}
-if (numero == 7){
-    alert ("hoje e sabado")
-}
+let distancia = Number (prompt( "digite a distancia em Km: " )) 
+let Tempo
+let velocidade = 300000 
+tempo = distancia / velocidade 
+alert (' A viagem vai durar ' + tempo + ' segundos!');
+
+
